db92408ff385c32460e9e38e511450d89ee47b41
OliverKarger <kmaster@oliver-karger.de>
2021-07-28 19:06:05 +0200

    dist/ Ordner gelöscht
    Signed-off-by: OliverKarger <kmaster@oliver-karger.de>

--- dist/libs/sql-tools/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/Select';
-export * from './lib/Insert';
--- dist/libs/sql-tools/src/lib/Insert.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type InsertValuePair = {
-    fieldName: string;
-    fieldValue: string;
-};
-export declare type params = {
-    table: string;
-    pairs: InsertValuePair[];
-};
-export declare function MySQL_Insert(props: params): string;
